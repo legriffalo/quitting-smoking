22a509c08edfe22d14b00eb58e6ce4500c1c7d24
Nick-legriffalo-Grif <56260217+legriffalo@users.noreply.github.com>
2024-08-21 17:19:19 +0300

    rewrite

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register (
-      '/quitting-smoking/service_worker.js',
-      {scope: '/quitting-smoking/'}
-    )
-  }
+  navigator.serviceWorker.register (
+    'service_worker.js',
+    {scope: '/'}
+  )
+}
--- service_worker.js
+++ service_worker.js
@@ -1,5 +1,5 @@
 // Change this to your repository name
-var GHPATH = '/quitting-smoking';
+var GHPATH = '/';
  
 // Choose a different app prefix name
 var APP_PREFIX = 'qs_';
@@ -15,16 +15,47 @@ var CACHE_NAME = APP_PREFIX + VERSION
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
-  '/quitting-smoking/',
-  `${GHPATH}/stopsmoking.html`,
-  `${GHPATH}/css/main.css`,
-  `${GHPATH}/js/main.js`,
-  `${GHPATH}/js/calendar.js`,
-  `${GHPATH}/js/data.js`,
-  `${GHPATH}/js/index.js`
+  // '/',
+  `${GHPATH}stopsmoking.html`,
+  `${GHPATH}css/main.css`,
+  `${GHPATH}js/main.js`,
+  `${GHPATH}js/calendar.js`,
+  `${GHPATH}js/data.js`,
+  `${GHPATH}js/index.js`
   // `${GHPATH}/dist/**`
 
 ]
+
+
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll(URLS);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+
+          // Save the resource in the cache and return it.
+          // cache.add(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
+});
 
 // code to delete caches
 
@@ -44,43 +75,43 @@ var URLS = [
 //     );
 //   });
 
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url);
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { 
-          console.log('responding with cache : ' + e.request.url);
-          return request
-        } else {       
-          console.log('file is not cached, fetching : ' + e.request.url);
-          return fetch(e.request)
-        }
-      })
-    )
-  })
+// self.addEventListener('fetch', function (e) {
+//     console.log('fetch request : ' + e.request.url);
+//     e.respondWith(
+//       caches.match(e.request).then(function (request) {
+//         if (request) { 
+//           console.log('responding with cache : ' + e.request.url);
+//           return request
+//         } else {       
+//           console.log('file is not cached, fetching : ' + e.request.url);
+//           return fetch(e.request)
+//         }
+//       })
+//     )
+//   })
   
-  self.addEventListener('install', function (e) {
-    e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
-        console.log('installing cache : ' + CACHE_NAME);
-        return cache.addAll(URLS)
-      })
-    )
-  })
+//   self.addEventListener('install', function (e) {
+//     e.waitUntil(
+//       caches.open(CACHE_NAME).then(function (cache) {
+//         console.log('installing cache : ' + CACHE_NAME);
+//         return cache.addAll(URLS)
+//       })
+//     )
+//   })
   
-  self.addEventListener('activate', function (e) {
-    e.waitUntil(
-      caches.keys().then(function (keyList) {
-        var cacheWhitelist = keyList.filter(function (key) {
-          return key.indexOf(APP_PREFIX)
-        })
-        cacheWhitelist.push(CACHE_NAME);
-        return Promise.all(keyList.map(function (key, i) {
-          if (cacheWhitelist.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i] );
-            return caches.delete(keyList[i])
-          }
-        }))
-      })
-    )
-  })
+//   self.addEventListener('activate', function (e) {
+//     e.waitUntil(
+//       caches.keys().then(function (keyList) {
+//         var cacheWhitelist = keyList.filter(function (key) {
+//           return key.indexOf(APP_PREFIX)
+//         })
+//         cacheWhitelist.push(CACHE_NAME);
+//         return Promise.all(keyList.map(function (key, i) {
+//           if (cacheWhitelist.indexOf(key) === -1) {
+//             console.log('deleting cache : ' + keyList[i] );
+//             return caches.delete(keyList[i])
+//           }
+//         }))
+//       })
+//     )
+//   })
